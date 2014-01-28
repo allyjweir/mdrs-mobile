3a4c4e5cf7a3282715eda6367765c9231254c077
Ally Weir <ally.pcgf@gmail.com>
2014-01-28 22:52:34 +0000

    Revert "messed stuff up"
    This reverts commit bfeaefce29c072b7db7ad8e0c74b7183ca68e8ba.

--- MDRS/gen/ggow/teamt/mdrs/R.java
+++ MDRS/gen/ggow/teamt/mdrs/R.java
@@ -425,7 +425,10 @@ containing a value of this type.
         public static final int AppTheme=0x7f0b0001;
     }
     public static final class xml {
-        public static final int preferences=0x7f040000;
+        public static final int pref_data_sync=0x7f040000;
+        public static final int pref_general=0x7f040001;
+        public static final int pref_headers=0x7f040002;
+        public static final int pref_notification=0x7f040003;
     }
     public static final class styleable {
         /** Attributes that can be used with a AdsAttrs.
