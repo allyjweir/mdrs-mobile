c19875e1a897d5f1bd43f94579e7f6490046aa16
Ally Weir <ally.pcgf@gmail.com>
2014-03-19 19:58:47 +0000

    Added placeholder image for logged in user (User functionality is still incomplete) and working on tweaking the upload screen layout slightly

--- MDRS/gen/ggow/teamt/mdrs/R.java
+++ MDRS/gen/ggow/teamt/mdrs/R.java
@@ -238,16 +238,17 @@ containing a value of this type.
     }
     public static final class id {
         public static final int GridLayout1=0x7f06000a;
-        public static final int action_cancel=0x7f060013;
-        public static final int action_confirm=0x7f060014;
-        public static final int action_feedback=0x7f060011;
-        public static final int action_settings=0x7f060012;
+        public static final int action_cancel=0x7f060014;
+        public static final int action_confirm=0x7f060015;
+        public static final int action_feedback=0x7f060012;
+        public static final int action_settings=0x7f060013;
         public static final int button_capture=0x7f060007;
         public static final int camera_preview=0x7f060009;
-        public static final int desc=0x7f06000e;
-        public static final int gallery1=0x7f06000b;
+        public static final int desc=0x7f06000f;
+        public static final int gallery1=0x7f06000c;
         public static final int hybrid=0x7f060004;
-        public static final int logged_in_as=0x7f06000f;
+        public static final int imageGallery=0x7f06000b;
+        public static final int logged_in_as=0x7f060010;
         public static final int map=0x7f060005;
         public static final int none=0x7f060000;
         public static final int normal=0x7f060001;
@@ -255,9 +256,9 @@ containing a value of this type.
         public static final int satellite=0x7f060002;
         public static final int stopRecording=0x7f060008;
         public static final int terrain=0x7f060003;
-        public static final int title=0x7f06000d;
-        public static final int uploadScreenMap=0x7f06000c;
-        public static final int user_image=0x7f060010;
+        public static final int title=0x7f06000e;
+        public static final int uploadScreenMap=0x7f06000d;
+        public static final int user_image=0x7f060011;
     }
     public static final class integer {
         public static final int google_play_services_version=0x7f080000;
--- MDRS/src/ggow/teamt/mdrs/login_dialog.java
+++
@@ -1,5 +0,0 @@
-package ggow.teamt.mdrs;
-
-public class login_dialog {
-
-}
